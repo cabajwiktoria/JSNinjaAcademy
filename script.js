a98f000a261664487201c338b3ba72937b5b79eb
Wiktoria Cabaj <BCNK43@motorolasolutions.com>
2021-05-12 11:18:30 +0200

    String editt

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const firstName = 'Wiktoria';
-const job = 'tester';
+const job = 'QA';
 const birthYear = 1994;
 const year = 2021
 
 
-const wiktoria = `I'am ${firstName}, a have ${year - birthYear} years and I work as ${job}!`;
+const wiktoria = `I'am ${firstName}, I have ${year - birthYear} years and I work as ${job}!`;
 console.log(wiktoria);
