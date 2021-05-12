42266527fa6ac1e459e155dc26b49023c5680eba
Wiktoria Cabaj <BCNK43@motorolasolutions.com>
2021-05-12 11:08:17 +0200

    Adding my project for JS Ninja Academy

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName = 'Wiktoria';
+const job = 'tester';
+const birthYear = 1994;
+const year = 2021
+
+
+const wiktoria = `I'am ${firstName}, a have ${year - birthYear} years old and I wark as ${job}!`;
+console.log(wiktoria);
