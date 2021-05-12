ca10be62658b0454e8fe62f65787676b75dc37ff
Wiktoria Cabaj <BCNK43@motorolasolutions.com>
2021-05-12 11:26:37 +0200

    Editing string

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// zmienne 
 const firstName = 'Wiktoria';
+const lastName = 'Cabaj';
 const job = 'QA';
 const birthYear = 1994;
 const year = 2021
 
-
-const wiktoria = `I'am ${firstName}, I have ${year - birthYear} years and I work as ${job}!`;
+// string 
+const wiktoria = `I'am ${firstName} ${lastName}, I have ${year - birthYear} years and I work as ${job}!`;
 console.log(wiktoria);
+
+// typy danych
+console.log(typeof (firstName));
+console.log(typeof (lastName));
+console.log(typeof (job));
+console.log(typeof (birthYear));
+console.log(typeof (year));
+console.log(typeof (wiktoria));
