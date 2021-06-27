91ae3b7488594bdf7c16733239fa9fa3e770b117
Wiktoria Cabaj <BCNK43@motorolasolutions.com>
2021-06-27 20:50:58 +0200

    Improvements

--- blog/script.js
+++ blog/script.js
@@ -1,36 +1,40 @@
 "use strict";
 
 const posts = [];
-function send() {
-  const title = document.getElementById("post-title").value;
-  const text = document.getElementById("post-text").value;
-  const author = document.getElementById("post-author").value;
-  const post = new Post(title, text, author);
-  posts.push(post);
-  renderPosts(posts);
-}
 
-function Post(title, text, author, remove) {
-  this.title = title;
-  this.text = text;
-  this.author = author;
-  this.remove = "Remove this post";
-  this.getListObject = function () {
-    const li = document.createElement("li");
-    const h3 = document.createElement("h3");
-    const pText = document.createElement("p");
-    const pAuthor = document.createElement("p");
-    const remove = document.createElement("button");
-    h3.innerText = this.title;
-    pText.innerText = this.text;
-    pAuthor.innerText = this.author;
-    remove.innerText = this.remove;
-    li.appendChild(h3);
-    li.appendChild(pText);
-    li.appendChild(pAuthor);
-    li.appendChild(remove);
-    return li;
-  };
+class Post {
+  constructor(title, text, author) {
+    this.title = title;
+    this.text = text;
+    this.author = author;
+    this.remove = "Remove post";
+    this.index = Date.now();
+    this.getListObject = function () {
+      const li = document.createElement("li");
+      const h3 = document.createElement("h3");
+      const pText = document.createElement("p");
+      const pAuthor = document.createElement("p");
+      const button = document.createElement("button");
+      h3.innerText = this.title;
+      pText.innerText = this.text;
+      pAuthor.innerText = this.author;
+      button.innerText = this.remove;
+      li.id = this.index;
+      button.onclick = () => {
+        let currentIndex = this.index;
+        for (let i = 0; i < posts.length; i++)
+          if (posts[i].index == this.index) {
+            let currentPost = document.getElementById(currentIndex);
+            currentPost.remove();
+          }
+      };
+      li.appendChild(h3);
+      li.appendChild(pText);
+      li.appendChild(pAuthor);
+      li.appendChild(button);
+      return li;
+    };
+  }
 }
 
 function renderPosts() {
@@ -41,4 +45,13 @@ function renderPosts() {
     const li = post.getListObject();
     list.appendChild(li);
   }
+}
+
+function send() {
+  const title = document.getElementById("post-title").value;
+  const text = document.getElementById("post-text").value;
+  const author = document.getElementById("post-author").value;
+  const post = new Post(title, text, author);
+  posts.push(post);
+  renderPosts(posts);
 }
